03b9e05f2da38eb93ad0d6b9f3efc386a33dedae
drighetto <drighetto@excellium-services.com>
2025-02-01 21:04:40 +0100

    Work on https://github.com/OWASP/www-project-secure-headers/discussions/210

---
+++ ci/tab_stats_manage_generation.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#########################################################################
+# This script manage the generation/update of the tab represented by the 
+# file "tab_stats.md".
+#########################################################################
+DATA_DB_FILE_LOCATION="https://github.com/oshp/oshp-stats/raw/refs/heads/main/data/data.db"
+DATA_DB_FILE="/tmp/data.db"
+echo "[+] Download the database of headers analysis..."
+wget -q -O $DATA_DB_FILE $DATA_DB_FILE_LOCATION
+file $DATA_DB_FILE
+chmod +x tab_stats_generate_*
+echo "[+] Generate the MD file of the TAB and all the MMD files for every pie chart image..."
+python tab_stats_generate_md_file.py
+echo "[+] Generate the PNG image corresponding to each MMD file..."
+bash tab_stats_generate_png_files.sh
+echo "[+] Cleanup"
+rm $DATA_DB_FILE
