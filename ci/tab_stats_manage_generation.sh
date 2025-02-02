d804c803a3e65f692f2a0bcf434c120b80bb273c
drighetto <drighetto@excellium-services.com>
2025-02-02 17:23:02 +0100

    Fix error - Work on https://github.com/OWASP/www-project-secure-headers/discussions/210

--- ci/tab_stats_manage_generation.sh
+++ ci/tab_stats_manage_generation.sh
@@ -5,9 +5,11 @@
 #########################################################################
 DATA_DB_FILE_LOCATION="https://github.com/oshp/oshp-stats/raw/refs/heads/main/data/data.db"
 DATA_DB_FILE="/tmp/data.db"
-echo "[+] Download the database of headers analysis..."
+IMAGE_FOLDER_LOCATION="../assets/tab_stats_generated_images"
+echo "[+] Download the database of headers analysis anc validate the database file..."
 wget -q -O $DATA_DB_FILE $DATA_DB_FILE_LOCATION
 file $DATA_DB_FILE
+sqlite3 $DATA_DB_FILE ".tables"
 echo "[+] Set correct access rights for the scripts as well as UNIX CRLF settings..."
 dos2unix tab_stats_generate_*
 chmod +x tab_stats_generate_*
@@ -17,3 +19,13 @@ echo "[+] Generate the PNG image corresponding to each MMD file..."
 bash tab_stats_generate_png_files.sh
 echo "[+] Cleanup"
 rm $DATA_DB_FILE
+echo "[+] Check correct generation of the images..."
+img_count=$(find $IMAGE_FOLDER_LOCATION -name "*.png" | wc -l)
+if [ $img_count -eq 0 ]
+then
+    echo "[!] No image file was generated!"
+    exit 1
+else
+    echo "[V] $img_count image files were generated!"
+    exit 0
+fi
