9d08e54252a55c839a4d96a4960f2c6e5dc53542
righettod <dominique.righetto@gmail.com>
2026-06-21 09:31:12 +0200

    Migrate content following https://github.com/OWASP/www-project-secure-headers/discussions/273

--- ci/common.py
+++ ci/common.py
@@ -1,4 +1,4 @@
 #########################################################
 # Common functions and constants used by all CI scripts
 #########################################################
-TOP_NAVIGATION_LINKS = "[Index](00_index.md) | [Response Headers](01_headers.md) | [Browser Support](02_browser_support.md) | [Best Practices](03_best_practices.md) | [Technical Resources](04_technical_resources.md) | [Code Snippets](05_code_snippets.md) | [Miscellaneous](06_misc.md) | [Statistics](07_statistics.md) | [Case Studies](08_case_studies.md) | [Logo](09_logo.md) | [Monitoring Technical Teferences Dashboard](10_monitoring_technical_references_dashboard.md)"
+TOP_NAVIGATION_LINKS = "[Index](00_index.md) | [Response Headers](01_headers.md) | [Browser Support](02_browser_support.md) | [Best Practices](03_best_practices.md) | [Technical Resources](04_technical_resources.md) | [Code Snippets](05_code_snippets.md) | [Miscellaneous](06_misc.md) | [Statistics](07_statistics.md) | [Case Studies](08_case_studies.md) | [Logo](09_logo.md) | [Monitoring Technical References Dashboard](10_monitoring_technical_references_dashboard.md)"
