350c21c6905c3ae205c738924e7f701b349965f8
righettod <dominique.righetto@gmail.com>
2026-06-22 16:57:35 +0200

    Migrate content following https://github.com/OWASP/www-project-secure-headers/discussions/273

--- ci/monitoring_oshp_gha_workflows.sh
+++ ci/monitoring_oshp_gha_workflows.sh
@@ -3,7 +3,7 @@
 # Audit all GHA (GitHub Actions) workflows with Plumber.
 #
 # Dependencies:
-#   apt install curl 
+#   apt install curl jq
 #
 # We use a fixed version to apply control on the version used and
 # decrease exposure if the repo of plumber is compromised.
@@ -39,5 +39,14 @@ $PLUMBER_BINARY_LOCATION config validate
 echo "[+] Audit the GHA workflows..."
 rm -f $PLUMBER_REPORT_LOCATION 2>/dev/null
 $PLUMBER_BINARY_LOCATION analyze --sarif $PLUMBER_REPORT_LOCATION
+# Currently the disabling of the check on protected branch is not taken in account so the
+# following execution error is raised:
+# "data collection incomplete: branch protection could not be fetched; branch controls were not evaluated"
+# So we update the attribute "invocations[0].executionSuccessful" to true if the report was correctly generated
+if [ -s "$PLUMBER_REPORT_LOCATION" ]
+then
+  jq '(.runs[].invocations[].executionSuccessful) = true' $PLUMBER_REPORT_LOCATION
+fi
+cat $PLUMBER_REPORT_LOCATION | jq
 # Force exit 0 because plumber return an exit code 3 because an access token is not provided
 exit 0
